179f3477a5d7cd40a18a49b588f021cbce0c85c8
pradadimas534@gmail.com <pradanadimas534@gmail.com>
2024-11-25 09:30:11 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/pemrograman_asynchronous/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pemrograman_asynchronous
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,126 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+// import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({
+    super.key,
+  });
+
+  // final String title;
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+
+  Future<http.Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const Path = '/books/v1/volumes/B1mxDwAAQBAJ';
+    Uri url = Uri.https(authority, Path);
+    return http.get(url);
+  }
+
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future Count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
+  }
+
+  late Completer completer;
+
+  Future getNumber() {
+    completer = Completer<int>();
+    calculate();
+    return completer.future;
+  }
+
+  Future calculate() async {
+    try{
+      await Future.delayed(const Duration(seconds: 5));
+      completer.complete(42);
+    }
+    catch (_) {
+      completer.completeError({});
+    }
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future'),
+      ),
+      body: Center(
+        child: Column(children: [
+          const Spacer(),
+          ElevatedButton(
+            child: const Text('GO!'),
+            onPressed: () {
+              setState(() {});
+              getNumber().then((value) {
+                // result = value.body.toString().substring(0, 450);
+                setState(() {
+                  result = value.toString();
+                });
+              }).catchError((_) {
+                result = 'An error occurred';
+                // setState(() {});
+              });
+              // Count();
+            },
+          ),
+          const Spacer(),
+          Text(result),
+          const Spacer(),
+          const CircularProgressIndicator(),
+          const Spacer(),
+        ]),
+      ),
+    );
+  }
+}
